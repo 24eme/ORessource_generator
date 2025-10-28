3985232ec00afd319249d96840f81b1264c4b726
tale-fau <tale-fau@24eme.fr>
2025-10-28 11:34:06 +0100

    Ajotu layout et partials

---
+++ app/views/layout.html.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ORessource - Plateforme open source de génération d'instance du logiciel ORessource pour les ressourceries</title>
+  <meta name="description" content="Plateforme open source de génération d'instance du logiciel ORessource pour les ressourceries" />
+  <link rel="icon" type="image/png" sizes="16x16" rel="noopener" target="_blank" href="/images/favicons/favicon_16x16.png">
+  <link rel="icon" type="image/png" sizes="32x32" rel="noopener" target="_blank" href="/images/favicons/favicon_32x32.png">
+  <link rel="apple-touch-icon" sizes="180x180" rel="noopener" target="_blank" href="/images/favicons/apple_touch_icon_180x180.png">
+  <link rel="manifest" href="/images/favicons/site.webmanifest">
+  <link href="/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="/css/bootstrap-icons.min.css" rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href="/css/common.css" rel="stylesheet" />
+</head>
+<body style="height: 83vh;">
+  <?php include($PARTIALS.'/_header.html.php'); ?>
+  <div class="bg-light" style="height: 100%;">
+    <?php include($content); ?>
+</div>
+<?php include($PARTIALS.'/_footer.html.php'); ?>
+</body>
+
+<script src="/js/jquery-3.7.1.min.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
+<script src="/js/main.js"></script>
+</html>
