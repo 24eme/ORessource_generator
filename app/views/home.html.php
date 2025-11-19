d8a6e7df89187dd06c810ab737e6d1c26aae8bd2
Tangui Morlier <tangui@tangui.eu.org>
2025-11-19 15:54:33 +0100

    Nouveau logo

--- app/views/home.html.php
+++ app/views/home.html.php
@@ -18,13 +18,13 @@
   <div class="bg-white" style="border-bottom: 1px solid #ddd">
     <div class="container" style="max-width: 1050px;">
       <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
-        <div class="col-md-1 mb-2 mb-md-0">
+        <div class="col-md-3">
           <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-            <img src="/images/logo_oressource.svg" height="80" />
+            <img src="/images/logo_oressource.svg" width="200" />
           </a>
         </div>
 
-        <ul class="nav col-md-8 col-md-auto mb-2 justify-content-center mb-md-0">
+        <ul class="nav col-md-6 col-md-auto mb-2 justify-content-center mb-md-0">
           <li><a href="#accueil" class="nav-link px-2 link-secondary">Accueil</a></li>
           <li><a href="#projet" class="nav-link px-2">Le projet</a></li>
           <li><a href="#fonctionnalites" class="nav-link px-2">Fonctionnalités</a></li>
