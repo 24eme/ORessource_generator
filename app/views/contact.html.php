5b7afc6848f9d08ad9940478f1b56522406d1110
Tangui Morlier <tangui@tangui.eu.org>
2025-11-20 18:28:14 +0100

    Template de Contact

---
+++ app/views/contact.html.php
@@ -0,0 +1,14 @@
+<div class="container mt-5">
+<div class="row">
+<div class="p5 m5 card shadow-sm">
+	<h1>Contacter l'équipe</h1>
+	<p>oressource.org est un projet proposé par la coopérative 24ème.</p>
+    <p>Pour toute demande d'information sur le projet, de maintenance sur ORessource ou d'éléments liés à l'hébergement, vous pouvez contacter son équipe par les moyens suivants :</p>
+	<ul>
+        <li><i class="bi bi-envelope"></i>&nbsp;Email : <a href="mailto:equipe@24eme.fr">equipe@24eme.fr</a></li>
+        <li><i class="bi bi-telephone"></i>&nbsp;Téléphone : <a href="tel:+33184259401">01 84 25 94 01</a></li>
+		<li><i class="bi bi-chat"></i>&nbsp;Chat : <a href="https://web.libera.chat/#24eme">#24eme <small>(irc.libera.chat:6697)</small></a></li>
+    </ul>
+</div>
+</div>
+</div>
