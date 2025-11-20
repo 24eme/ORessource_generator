c0370923ee95a6066c5ce9b10bcd8d7d59cc559d
tale-fau <tale-fau@24eme.fr>
2025-11-20 18:10:39 +0100

    Changement vocabulaire

--- app/views/create.html.php
+++ app/views/create.html.php
@@ -8,7 +8,7 @@
       </nav>
       <a name="accueil"></a>
         <div id="mainInfo" class="card mt-4 shadow-sm">
-        <h5 class="card-header">Informations de la ressourcerie</h5>
+        <h5 class="card-header">Informations de la structure</h5>
           <div class="card-body">
           <?php foreach(\Flash::instance()->getMessages() as $message): ?>
             <div class="alert alert-<?php echo $message['status']?> alert-dismissable">
@@ -18,15 +18,15 @@
           <form id="formInfo" class="needs-validation" action="/dataCheck" method="post" enctype="multipart/form-data">
             <div class="row g-3">
               <div class="col-8">
-                <label for="nomRessourcerie" class="form-label">Le nom de votre ressourcerie</label>
-                <input type="text" class="form-control" id="nomRessourcerie" name="nomRessourcerie" placeholder="Ma ressourcerie" value="<?php if (array_key_exists('nomRessourcerie', $SESSION)){echo $SESSION['nomRessourcerie'];} ?>" required>
+                <label for="nomRessourcerie" class="form-label">Le nom de votre structure</label>
+                <input type="text" class="form-control" id="nomRessourcerie" name="nomRessourcerie" placeholder="Ma structure" value="<?php if (array_key_exists('nomRessourcerie', $SESSION)){echo $SESSION['nomRessourcerie'];} ?>" required>
                 <div class="invalid-feedback">
-                  Nom de la ressourcerie nécessaire
+                  Nom de la structure nécessaire
                 </div>
               </div>
 
               <div class="col-8">
-                <label for="adresseRessourcerie" class="form-label">Adresse de votre ressourcerie</label>
+                <label for="adresseRessourcerie" class="form-label">Adresse de votre structure</label>
                 <input type="text" class="form-control" id="adresseRessourcerie" name="adresseRessourcerie" placeholder="24 rue du zéro déchet" value="<?php if (array_key_exists('adresseRessourcerie', $SESSION)){echo $SESSION['adresseRessourcerie'];} ?>" required>
                 <div class="invalid-feedback">
                   Adresse nécessaire
@@ -52,7 +52,7 @@
 
               <div class="col-8">
                 <label for="email" class="form-label">Adresse email</small></label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="vous@maressourcerie.fr" value="<?php if (array_key_exists('emailRessourcerie', $SESSION)){echo $SESSION['emailRessourcerie'];} ?>" required>
+                <input type="email" class="form-control" id="email" name="email" placeholder="vous@mastructure.fr" value="<?php if (array_key_exists('emailRessourcerie', $SESSION)){echo $SESSION['emailRessourcerie'];} ?>" required>
                 <p class="text-muted">Cette adresse email vous servira d'identifiant administrateur.ice</p>
                 <div class="invalid-feedback">
                   Merci d'entrer une adresse mail valide, qui sera votre moyen de connexion au logiciel
